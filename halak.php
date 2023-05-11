6cd27ea73138e8ca34270145af10ee960c35146d
kriswell1 <kristofattila1975.0@gmail.com>
2023-05-11 12:54:41 +0200

    2.állapot
    Halak.php oldal + images mappa + egy kép +index.html cserélve index.php-re. Teszt adatbázis nem működött mert rosszul lett exportálva

---
+++ halak.php
@@ -0,0 +1,83 @@
+<?php
+$db_name='teszt';
+
+$db_pass='VsRSAs/cJB1vIfpv';
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>header</header>
+    <nav>
+        <ul>
+            <li>
+                <a href="index.php">Kezdőlap</a>
+            </li>
+            <li>
+                <a href="halak.php">Díszhalak</a>
+            </li>
+            <li>
+                <a href="kutyak.php">Kutyák</a>
+            </li>
+            <li>
+                <a href="macskak.php">Macskák</a>
+            </li>
+            <li>
+                <a href="madarak.php">Madarak</a>
+            </li>
+        </ul>
+    </nav>
+    <aside>aside
+        <section>section1</section>
+        <section>section2</section>
+    </aside>
+    <main id="halak">
+    
+    <div>
+        <img src="./images/neon.png" alt="neonhal"><br>
+        <span>Neonhal</span>   <span>Ár:200 FT</span>
+       
+    </div>
+
+    <div>
+        <img src="./images/vitorlas.jpg" alt="Vitorláshal"><br>
+        <span>Vitorláshal</span>   <span>Ár:500 FT</span>
+        
+
+    </div>
+    <div>
+        <img src="https://vadvilagbolt.cdn.shoprenter.hu/custom/vadvilagbolt/image/data/labirintkopolty%C3%BAs/t%C3%B6rpe%20gur%C3%A1mi.jpg?lastmod=1682422198.1625440538" alt="Törpe gurámi"><br>
+        <span>Törpe gurámi</span>   <span>Ár:1500 FT</span>
+    </div>
+
+    <div>
+    <img src="http://akvaguru.hu/kepek2/halak/panga2.jpg" alt="neonhal"><br>
+        <span>Ázsiai cápaharcsa</span>  <span>Ár:2500 FT</span>
+    </div> 
+    <div>
+    <img src="https://www.fisch.hu/images/cikkek_akva/thumb/crop400x266/xifo.jpg" alt="xifó"><br>
+        <span>Szifó</span>  <span>Ár:1200 FT</span>
+    </div> 
+    <div>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Pygocentrus_nattereri_01.jpg/260px-Pygocentrus_nattereri_01.jpg" alt="Vöröshasú pirája"><br>
+        <span>Vöröshasú pirája</span>  <span>Ár:4500 FT</span>
+    </div> 
+    <div>
+    <img src="https://www.haziallat.hu/upload/4/article/3411/diszkoszhalak.jpg" alt="Diszkoszhal"><br>
+        <span>Diszkoszhal</span>  <span>Ár:4500 FT</span>
+    </div> 
+    </main>
+    <footer>footer</footer>
+<?php ?>
+</body>
+</html>
